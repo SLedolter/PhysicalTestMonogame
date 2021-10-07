f1daaec1c6697a25d2b566eda41e23e497c2aeea
Stefan Ledolter <stefan.ledolter@ulrich.at>
2021-10-07 15:48:06 +0200

    SL: adding content tutorial

--- PhysicalTestMonogame/Game1.cs
+++ PhysicalTestMonogame/Game1.cs
@@ -7,6 +7,8 @@ namespace PhysicalTestMonogame {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
+    Texture2D ballTexture;
+
     public Game1() {
       _graphics = new GraphicsDeviceManager(this);
       Content.RootDirectory = "Content";
@@ -22,7 +24,7 @@ namespace PhysicalTestMonogame {
     protected override void LoadContent() {
       _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-      // TODO: use this.Content to load your game content here
+      ballTexture = Content.Load<Texture2D>("ball");
     }
 
     protected override void Update(GameTime gameTime) {
@@ -37,7 +39,9 @@ namespace PhysicalTestMonogame {
     protected override void Draw(GameTime gameTime) {
       GraphicsDevice.Clear(Color.CornflowerBlue);
 
-      // TODO: Add your drawing code here
+      _spriteBatch.Begin();
+      _spriteBatch.Draw(ballTexture, new Vector2(0, 0), Color.White);
+      _spriteBatch.End();
 
       base.Draw(gameTime);
     }
